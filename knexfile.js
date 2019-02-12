24199bf4f6e976f46e3a4da3e91333b94ce2f0a1
Jesper947 <Jesper.svennebring@valtech.com>
2019-02-12 11:43:01 +0100

    a lot

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 // Update with your config settings.
 
 module.exports = {
@@ -11,11 +12,12 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    // connection: {
+    //   database: 'my_db',
+    //   user: 'username',
+    //   password: 'password'
+    // },
+    connection: `${process.env.DATABASE_URL}${process.env.NODE_ENV === 'production' ? '?ssl=require' : ''}`,
     pool: {
       min: 2,
       max: 10
